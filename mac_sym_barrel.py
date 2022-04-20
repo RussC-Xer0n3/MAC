dba24480ca9fa4a01d5830c554e178b97513b2ae
Xer0n3 - RussC <developing821@outlook.com>
2022-04-20 20:28:53 +0100

    Old Files
    Files from old account russellclarke82@gmail.com

---
+++ mac_sym_barrel.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# Simple to increment some values.
+# @author Russell Clarke 0ka Rusher SD200984
+# @version 1.7
+# @Created 21.12.2018
+# @Listening to Talvin Singh
+
+h = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+for a in h:
+    for b in h:
+        for c in h:
+            for d in h:
+                for e in h:
+                    for f in h:
+                        for g in h:
+                            for h in h:
+                                for i in h:
+                                    for j in h:
+                                        for k in h:
+                                            for l in h:
+                                                print(l + k + ':' + j + i + ':' + h + g + ':' + f + e + ':' + d + c + ':' + b + a)      
+
+exit(0)
